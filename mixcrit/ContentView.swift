45af0da8407e4bb6668e0e779134293bb6d85339
xingxiaolin <xing.xiaolin@foxmail.com>
2026-05-07 10:10:30 +0800

    Initial Commit

---
+++ mixcrit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mixcrit
+//
+//  Created by 邢小林 on 2026/5/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mixcrit/mixcritApp.swift
@@ -0,0 +1,17 @@
+//
+//  mixcritApp.swift
+//  mixcrit
+//
+//  Created by 邢小林 on 2026/5/7.
+//
+
+import SwiftUI
+
+@main
+struct mixcritApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
